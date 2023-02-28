7d965089a45f799fd68ff875b073a68cfdd6880b
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-02-28 09:23:33 -0500

    Unit Testing with Python

---
+++ tests1.py
@@ -0,0 +1,20 @@
+import unittest
+
+from prime import is_prime
+
+
+class Tests(unittest.TestCase):
+    def test_1(self):
+        """Check that 1 is not prime"""
+        self.assertFalse(is_prime(1))
+
+    def test_2(self):
+        # Check that 2 is not prime
+        self.assertTrue(is_prime(2))
+
+    def test_11(self):
+        self.assertTrue(is_prime(11))
+
+
+if __name__ == "__main__":
+    unittest.main()
