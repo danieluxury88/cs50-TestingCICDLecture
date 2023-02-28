8a9db378eab058f3eb42c0ddae1e80406858e84d
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-02-28 12:41:26 -0500

    UnitTest over html pages

--- airline0/users/tests.py
+++ airline0/users/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
+from django.db.models import Max
+from django.test import TestCase, Client
 
 # Create your tests here.
-from .models import Airport, Flight
+from .models import Airport, Flight, Passenger
 
 
 class FlightTestCase(TestCase):
@@ -38,3 +39,43 @@ class FlightTestCase(TestCase):
         a2 = Airport.objects.get(code="BBB")
         f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
         self.assertFalse(f.is_valid_flight())
+
+    def test_index(self):
+        c = Client()
+        response = c.get("/flight/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["flights"].count(), 3)
+
+    def test_valid_flight_page(self):
+        a1 = Airport.objects.get(code="AAA")
+        f = Flight.objects.get(origin=a1, destination=a1)
+
+        c = Client()
+        response = c.get(f"/flight/{f.id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_flight_page(self):
+        max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
+
+        c = Client()
+        response = c.get(f"/flight/{max_id + 1}")
+        self.assertEqual(response.status_code, 404)
+
+    def test_flight_page_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+        f.passengers.add(p)
+
+        c = Client()
+        response = c.get(f"/flight/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["passengers"].count(), 1)
+
+    def test_flight_page_non_passengers(self):
+        f = Flight.objects.get(pk=1)
+        p = Passenger.objects.create(first="Alice", last="Adams")
+
+        c = Client()
+        response = c.get(f"/flight/{f.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["non_passengers"].count(), 1)
